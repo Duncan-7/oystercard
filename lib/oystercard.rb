74b9eaab1b8c951b1a87ac214e0edd1ac76cfe97
Jack <jack3009@hotmail.co.uk>
2020-10-19 14:49:32 +0100

    Top_up method defined. Maximum limit set and integrated into top up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,15 @@
 class Oystercard
   attr_reader :balance
-  
+  MAXIMUM_LIMIT = 90
+
   def initialize
     @balance = 0
   end
+
+  def top_up(amount)
+    new_balance = @balance + amount
+    raise "Maximum limit exceeded" if new_balance > MAXIMUM_LIMIT
+    @balance = new_balance
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,17 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe 'topping up our oystercard' do
+    it "tops up our oyster card with the given amount" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+    it "raises an error when the maximum amount is exceeded" do
+      expect{ subject.top_up(91) }.to raise_error("Maximum limit exceeded")
+    end
+  end
+
+
 end
